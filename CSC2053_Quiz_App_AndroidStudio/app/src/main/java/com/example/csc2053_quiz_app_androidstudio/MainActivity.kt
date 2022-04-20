e49e307d923fbc069b39a4356a990659242ce8a8
jhunt8Villanova <jhunt8@villanova.edu>
2022-04-20 14:34:42 -0400

    added initial project files

---
+++ CSC2053_Quiz_App_AndroidStudio/app/src/main/java/com/example/csc2053_quiz_app_androidstudio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.csc2053_quiz_app_androidstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
