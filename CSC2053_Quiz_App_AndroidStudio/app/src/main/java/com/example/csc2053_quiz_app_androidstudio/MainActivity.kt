ab1f4860f1cd53545a3846ac5625a1bc73e723c6
qinghu1 <qingqinghu279@gmail.com>
2022-04-25 14:38:04 -0400

    Added button onclick listeners

--- CSC2053_Quiz_App_AndroidStudio/app/src/main/java/com/example/csc2053_quiz_app_androidstudio/MainActivity.kt
+++ CSC2053_Quiz_App_AndroidStudio/app/src/main/java/com/example/csc2053_quiz_app_androidstudio/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.csc2053_quiz_app_androidstudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            showToast()
+        }
+        button.setOnClickListener {
+            Toast.makeText(applicationContext,"CORRECT!",Toast.LENGTH_SHORT).show()
+        }
+
+        button2.setOnClickListener {
+            Toast.makeText(applicationContext, "INCORRECT!", Toast.LENGTH_SHORT).show()
+        }
+
+        button3.setOnClickListener {
+            Toast.makeText(applicationContext, "WRONGO!", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    fun showToast() {
+        Toast.makeText(applicationContext,"CORRECT!", Toast.LENGTH_SHORT).show()
     }
 }
