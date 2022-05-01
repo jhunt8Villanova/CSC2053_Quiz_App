52e99f22d3e273e74a74a9db11296c43f249cd98
qinghu1 <qingqinghu279@gmail.com>
2022-05-01 17:50:01 -0400

    Added questions, created "correct answer" activity and made "MainActivity" its parent.

--- CSC2053_Quiz_App_AndroidStudio/app/src/main/java/com/example/csc2053_quiz_app_androidstudio/MainActivity.kt
+++ CSC2053_Quiz_App_AndroidStudio/app/src/main/java/com/example/csc2053_quiz_app_androidstudio/MainActivity.kt
@@ -8,8 +8,17 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
 
     var questionNo = 0
-    var questions = listOf("What are the two official languages for Android development? \n\n A) Kotlin and Java \n\n B) Java and Python \n\n C) Kotlin and Python", "How do you define a function in Kotlin? \n\n A) void \n\n B) var \n\n C) function", "What is a variable used for? \n\n A) To contain data \n\n B) To insert a random value \n\n C) Don't know", "What does SDK stand for in Android SDK? \n\n A) Software Development Kit \n\n B) Software Development Kotlin \n\n C) Something Don't Know")
-    var rightAnswers = listOf(1, 2, 1, 1)
+    var questions = listOf("1. What is the capital of Romania? \n\n A. Craiova\n\n B. Cluj-Napoca\n\n C. Bucharest",
+            "2. What is the capital of Portugal? \n\n A. Lisbon \n\n B. Porto \n\n C. Faro",
+            "3. What is the capital of Argentina? \n\n A) Buenos Aires \n\n B) Rosario \n\n C) Cordoba",
+            "4. What is the capital of Canada? \n\n A) Toronto \n\n B) Ottawa \n\n C) Montreal",
+            "5. What is the capital of Thailand? \n\n A) Changmai \n\n B) Pattaya City \n\n C) Bangkok",
+            "6. What is the capital of Serbia? \n\n A) Novi Sad \n\n B) Belgrade \n\n C) Leskovac",
+            "7. What is the capital of Nigeria? \n\n A) Lagos \n\n B) Kano \n\n C) Abuja",
+            "8. What is the capital of Finland? \n\n A) Tampere \n\n B) Helsinki \n\n C) Turku",
+            "9. What is the capital of Switzerland? \n\n A) Zurich \n\n B) Lucerne \n\n C) Bern",
+            "10. What is the capital of Morocco? \n\n A) Rabat \n\n B) Casablanca \n\n C) Fes")
+    var rightAnswers = listOf(3, 1, 1, 2, 3, 2, 3, 2, 3, 1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
